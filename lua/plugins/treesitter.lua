bac46051a61004b12f379f0d429b94eeaa86a249
Eduardo Rosário <eduardojfrosario@gmail.com>
2026-04-25 00:19:20 +0100

    fix hte treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,20 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		run = ":TSUpdate",
+		lazy = false,
+		build = ":TSUpdate",
+		config = function()
+			local treesitter = require("nvim-treesitter")
+
+			treesitter.setup()
+
+			local group = vim.api.nvim_create_augroup("TreesitterSetup", { clear = true })
+			vim.api.nvim_create_autocmd("FileType", {
+				group = group,
+				callback = function(args)
+					pcall(vim.treesitter.start, args.buf)
+				end,
+			})
+		end,
 	},
 }
